ad8bb645128fdb8b834cb1bf4bdc3d567082de18
oo <oo@gmail.com>
2023-04-06 09:08:37 +0800

    合并错误

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -106,7 +106,6 @@ export function Settings(props: { closeSettings: () => void }) {
   const showUsage = !!accessStore.token || !!accessStore.accessCode;
 
   useEffect(() => {
-    checkUpdate();
     showUsage && checkUsage();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
