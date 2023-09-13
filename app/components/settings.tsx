34057c0cf7bc546260107614e0170c9f03ef29a3
oo <oo@gmail.com>
2023-09-13 09:37:20 +0800

    修复合并bug

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -49,6 +49,7 @@ import Locale, {
   getLang,
 } from "../locales";
 import { copyToClipboard } from "../utils";
+import { Path } from "../constant";
 import { Prompt, SearchService, usePromptStore } from "../store/prompt";
 import { ErrorBoundary } from "./error";
 import { InputRange } from "./input-range";
